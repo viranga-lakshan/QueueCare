f790bfbd28ad5cbea1935eaec62033be662e80f0
viranga-lakshan <virangalakshanz@gmail.com>
2026-03-09 23:32:03 +0530

    Initial Commit

---
+++ QueueCare/QueueCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueueCareApp.swift
+//  QueueCare
+//
+//  Created by test on 2026-03-09.
+//
+
+import SwiftUI
+
+@main
+struct QueueCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
